91bd050fb90e0436ca59f9e2fe49aeb0d09835f6
Andywang201605 <ztwang201605@gmail.com>
2023-09-25 22:43:14 +0800

    restructure gen_cal_soln function

--- gen_calibration_soln.py
+++ gen_calibration_soln.py
@@ -30,37 +30,39 @@ def main(args):
         inp_vis = args.vis_ms
     elif args.vis_uvfits:
         inp_vis = args.vis_uvfits.strip("uvfits") + "ms"
-        print("------> Convering UV Fits ({0}) to MS ({1})".format(args.vis_uvfits, inp_vis))
-        importuvfits(fitsfile=args.vis_uvfits, vis=inp_vis)
+        if not os.path.exists(inp_vis):
+            print("------> Convering UV Fits ({0}) to MS ({1})".format(args.vis_uvfits, inp_vis))
+            importuvfits(fitsfile=args.vis_uvfits, vis=inp_vis)
 
+    ### put all file names at the very beginning
     if inp_vis.endswith("/"): inp_vis=inp_vis[:-1]
     work_dir = "/".join(inp_vis.split("/")[:-1])
-
     averaged_vis = inp_vis.strip("ms") + "aver.ms"
-    print("------> Averaging MS ({0}) and saving to {1}".format(inp_vis, averaged_vis))
-
-    average(inp_vis, averaged_vis, timebin="10s", freqbin=1) # average it to 1 MHz
-
     four_pol_vis = averaged_vis.strip("ms") + "4pol.ms"
+    model_name = four_pol_vis.strip("ms") + "model"
+    bin_name = four_pol_vis.strip("ms") + "bin"
+    freq_name = four_pol_vis.replace(".ms", ".freq.npy")
+    smooth_npy = bin_name.strip("bin") + "smooth.npy"
+
+
+    print("------> Averaging MS ({0}) and saving to {1}".format(inp_vis, averaged_vis))
+    average(inp_vis, averaged_vis, timebin="10s", freqbin=1) # average it to 1 MHz
 
     print("------> Converting MS ({0}) to 4pol ({1})".format(averaged_vis, four_pol_vis))
     convert(averaged_vis, four_pol_vis)
 
-    model_name = four_pol_vis.strip("ms") + "model"
     print("------> Extracting sky model and saving to {0}".format(model_name))
     extract(
         four_pol_vis, pb_radii = 2.0, flux_cutoff = 0.005, spectral_index = -0.83,
         catalog_file=args.catalog, freq_cat=args.catfreq*1e6,
     )
 
-    bin_name = four_pol_vis.strip("ms") + "bin"
     calibrate_cmd = "{build_dir}/calibrate -minuv 200.0 -m {model} {vis} {bin_name}".format(model=model_name, vis=four_pol_vis, build_dir=args.build_dir, bin_name=bin_name)
     print("------> Calibrating using the sky model and saving soln to {0}\n------> Executing {1}".format(bin_name, calibrate_cmd))
     os.system(calibrate_cmd)
 
     print("------> Exporting frequency from measurement sets....")
     craco_ms = SimpleMeasurementSet(four_pol_vis)
-    freq_name = four_pol_vis.replace(".ms", ".freq.npy")
     np.save(freq_name, craco_ms.freqs)
 
     print("------> Fitting calibration solution...")
@@ -68,7 +70,6 @@ def main(args):
     plotdir = f"{work_dir}/bp_smooth/"
     if not os.path.exists(plotdir): os.makedirs(plotdir)
     bp_ = smooth_bandpass(bp, plotdir=plotdir, flagchan=args.flagchan)
-    smooth_npy = bin_name.strip("bin") + "smooth.npy"
     np.save(smooth_npy, bp_)
     
     if args.clean:
@@ -77,7 +78,6 @@ def main(args):
         # print(f"rm -r {work_dir}/*.ms")
         os.system(f"rm -r {work_dir}/*.ms")
         
-
     print("-------> All Done!  We can now apply the solution saved in the soln file - {0}".format(bin_name))
 
 if __name__ == '__main__':
